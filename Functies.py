e4510d46e76d9b6f011053b4a98bccd868f284cd
S!ka <55556421+Malinas015@users.noreply.github.com>
2021-09-28 13:03:40 +0200

    Create Functies.py

---
+++ Functies.py
@@ -0,0 +1,5 @@
+# Functies
+#def , daarna naam van de functie afsluiten met dubbele punt
+def print_mijn_naam(name1,name2):
+    print("Dag " + name1 + ", " + name2 + "!") # valt onder de functie
+print_mijn_naam("Christian", "Killa")
